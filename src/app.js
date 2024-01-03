2e42ef3c430d100aff3b82332d48836a29dc3cfb
Yash Chede <yashchede99@gmail.com>
2024-01-03 19:27:52 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -23,4 +23,9 @@ app.use(express.static("public"));
 // for CURD operations of cookies in browser from server
 app.use(cookieParser());
 
+// routes
+import userRouter from "./routes/user.routes.js";
+
+app.use("/api/v1/users", userRouter);
+
 export { app };
